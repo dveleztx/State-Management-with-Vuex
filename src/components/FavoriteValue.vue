57383b5f3b173e6010800d436eb03d3c58219871
David Velez <dveleztx@gmail.com>
2021-04-09 21:39:48 -0500

    Demonstrated how to use Map Helpers, from mapActions to mapGetters, with examples of each.

--- src/components/FavoriteValue.vue
+++ src/components/FavoriteValue.vue
@@ -1,14 +1,18 @@
 <template>
-  <h3>{{ counter }}</h3>
+  <h3>{{ norm }}</h3>
   <p>We do more...</p>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   computed: {
-    counter() {
-      return this.$store.getters.normalizedCounter;
-    }
+  //   counter() {
+  //     return this.$store.getters.normalizedCounter;
+    ...mapGetters({
+      norm: 'normalizedCounter'
+    })
   }
 };
 </script>
