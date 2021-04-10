d65cdf2bbd0a5d91a503709806b30dacd9c7fe44
David Velez <dveleztx@gmail.com>
2021-04-09 22:26:18 -0500

    Separated the incrementing store mutation logic into its own object, its own module, and imported it back into the store. This makes it easier to logically separate various types of 'store' mutation logic into their own modules without making it look too messy.

--- src/main.js
+++ src/main.js
@@ -3,15 +3,12 @@ import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-const store = createStore({
-  // This is similar to data()
+const counterModule = {
   state() {
     return {
-      counter: 0,
-      isLoggedIn: false
+      counter: 0
     };
   },
-  // Similar to methods
   mutations: {
     increment(state) {
       state.counter += 2;
@@ -19,9 +16,6 @@ const store = createStore({
     // Payload here can be anything, a number, string, or even an object
     increase(state, payload) {
       state.counter = state.counter + payload.value;
-    },
-    setAuth(state, payload) {
-      state.isLoggedIn = payload.isAuth;
     }
   },
   actions: {
@@ -35,12 +29,6 @@ const store = createStore({
         console.log(context);
         context.commit('increase', payload);
       }, 2000);
-    },
-    login(context) {
-      context.commit('setAuth', { isAuth: true });
-    },
-    logout(context) {
-      context.commit('setAuth', { isAuth: false });
     }
   },
   getters: {
@@ -56,7 +44,36 @@ const store = createStore({
         return 100;
       }
       return finalCounter;
+    }
+  }
+};
+
+const store = createStore({
+  modules: {
+    numbers: counterModule
+  },
+  // This is similar to data()
+  state() {
+    return {
+      isLoggedIn: false
+    };
+  },
+  // Similar to methods
+  mutations: {
+    setAuth(state, payload) {
+      state.isLoggedIn = payload.isAuth;
+    }
+  },
+  actions: {
+    login(context) {
+      context.commit('setAuth', { isAuth: true });
     },
+    logout(context) {
+      context.commit('setAuth', { isAuth: false });
+    }
+  },
+  // Similar to computed
+  getters: {
     isUserAuthenticated(state) {
       return state.isLoggedIn;
     }
